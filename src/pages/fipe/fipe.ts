ee89a73d3a115600cf8c4b15894ff3f7cd32de12
WendellArnald <wendell.ribeiro@topinformation.com.br>
2018-11-26 11:09:20 -0200

    Create page Fipe.

---
+++ src/pages/fipe/fipe.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-fipe',
+  templateUrl: 'fipe.html'
+})
+export class FipePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
+//import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { FipePage } from '../fipe/fipe';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,7 +11,7 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab2Root = FipePage;
   tab3Root = ContactPage;
 
   constructor() {
